d1ff6c7ef92afbfe852bf107f287806a89a5a31a
Joshua Topacio <joshuatopacio27@gmail.com>
2017-12-04 15:38:46 -0700

    added authentication

--- INTEX/Properties/AssemblyInfo.cs
+++ INTEX/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("20c14a03-f834-4501-90ef-24aa282556b2")]
+[assembly: Guid("f17ebce3-2876-4e95-8f18-3faecb7ebcbd")]
 
 // Version information for an assembly consists of the following four values:
 //
